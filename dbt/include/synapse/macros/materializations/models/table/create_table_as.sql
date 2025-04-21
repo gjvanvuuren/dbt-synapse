77740af263ffac7fde21b50c16fe4d8717abc7e3
Gideon Jansen van Vuuren <gvanvuuren@gmx.net>
2025-04-21 19:52:03 +0200

    Added required distribution to ctas.sql

--- dbt/include/synapse/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/synapse/macros/materializations/models/table/create_table_as.sql
@@ -1,14 +1,26 @@
 {% macro synapse__create_table_as(temporary, relation, sql) -%}
+    {%- set index = config.get('index', default="CLUSTERED COLUMNSTORE INDEX") -%}
+    {%- set dist = config.get('dist', default="ROUND_ROBIN") -%}
+
     {% set tmp_vw_relation = relation.incorporate(path={"identifier": relation.identifier ~ '__dbt_tmp_vw'}, type='view')-%}
+
     {% do adapter.drop_relation(tmp_vw_relation) %}
+
     {{ get_create_view_as_sql(tmp_vw_relation, sql) }}
 
     {% set contract_config = config.get('contract') %}
+
     {% if contract_config.enforced %}
 
         CREATE TABLE {{relation}}
         {{ build_columns_constraints(relation) }}
+        WITH(
+            DISTRIBUTION = {{dist}},
+            {{index}}
+        )
+
         {{ get_assert_columns_equivalent(sql)  }}
+
         {% set listColumns %}
             {% for column in model['columns'] %}
                 {{ "["~column~"]" }}{{ ", " if not loop.last }}
@@ -19,6 +31,6 @@
         SELECT {{listColumns}} FROM {{tmp_vw_relation}}
 
     {%- else %}
-        EXEC('CREATE TABLE {{relation}} AS SELECT * FROM {{tmp_vw_relation}}');
+        EXEC('CREATE TABLE {{relation}} WITH(DISTRIBUTION = {{dist}},{{index}}) AS SELECT * FROM {{tmp_vw_relation}}');
     {% endif %}
 {% endmacro %}
